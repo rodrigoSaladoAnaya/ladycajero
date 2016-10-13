f7ec15ecc9a7cf51454651bdd2a93d436180fd6c
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2016-10-13 11:53:31 -0500

    ref #1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const password = document.getElementById("password");
-password.addEventListener("keyup", function(e) {
+const psw = document.getElementById("psw");
+psw.addEventListener("keyup", function(e) {
   TweenLite.set("img", {x: function() {
-    return password.value.length * 14.8;
+    return psw.value.length * 14.8;
   }, opacity: function() {
-    return password.value.length / 10;
+    return psw.value.length / 10;
   }});
 });
