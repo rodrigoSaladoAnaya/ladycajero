72babb1ac6f2345425e270a853c3bafe2548d02b
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2016-10-13 11:25:56 -0500

    1rt commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const password = document.getElementById("password");
+password.focus();
+password.addEventListener("keyup", function(e) {
+  TweenLite.set("img", {x: function() {
+    return password.value.length * 14.8;
+  }, opacity: function() {
+    console.log(password.value.length / 10)
+    return password.value.length / 10;
+  }});
+});
