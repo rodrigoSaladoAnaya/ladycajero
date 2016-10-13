6ac1ceeab6e9e092e8bcf279d616ef6f5e647dfd
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2016-10-13 11:46:04 -0500

    clean code

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ password.addEventListener("keyup", function(e) {
   TweenLite.set("img", {x: function() {
     return password.value.length * 14.8;
   }, opacity: function() {
-    console.log(password.value.length / 10)
     return password.value.length / 10;
   }});
 });
